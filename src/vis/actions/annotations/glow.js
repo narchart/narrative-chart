897dc4dcbf3db981288e9814122a46ac0b939063
pairone <40026218+pairone@users.noreply.github.com>
2021-11-09 16:22:13 +0800

    add glow annotation (#13)
    * add glow annotation
    
    * update glow annotation

--- src/vis/actions/annotations/glow.js
+++ src/vis/actions/annotations/glow.js
@@ -1,8 +1,36 @@
+import Color from '../../visualization/color';
 import Annotator from './annotator'
 
 class Glow extends Annotator {
     annotate(chart, target) {
         let svg = chart.svg();
+        var defs = svg.append("defs");
+        var filter = defs.append("filter")
+            .attr("id", "drop-shadow")
+            .attr("height", "150%");
+
+        filter.append("feGaussianBlur")
+            .attr("in", "StrokePaint")
+            // .attr("in", "SourceAlpha")
+            .attr("stdDeviation", 2)
+            .attr("result", "blur");
+
+        filter.append("feOffset")
+            .attr("in", "blur")
+            .attr("dx", 0)
+            .attr("dy", 0)
+            .attr("result", "offsetBlur");
+
+        var feMerge = filter.append("feMerge");
+
+
+        feMerge.append("feMergeNode")
+            .attr("in", "offsetBlur")
+        feMerge.append("feMergeNode")
+            .attr("in", "SourceGraphic");
+        feMerge.append("feMergeNode")
+            .attr("in", "FillPaint");
+
         svg.selectAll(".mark")
             .filter(function(d) {
                 if (target.length === 0) {
@@ -14,7 +42,11 @@ class Glow extends Annotator {
                     }
                 }
                 return false
-            });
+            })
+            .style("stroke",  Color().ANNOTATION)
+            .style("stroke-width", 2)
+            .style("filter", "url(#drop-shadow)")
+            .attr("opacity", 1)
     }
 }
 
