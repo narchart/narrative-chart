ea7a4c8825b6e8afae0a7193ddfc3c5549468b75
pairone <40026218+pairone@users.noreply.github.com>
2021-11-24 23:41:24 +0800

    add texture (#30)
    * add texture
    
    * test new texture
    
    * test new texture

--- src/vis/actions/annotations/texture.js
+++ src/vis/actions/annotations/texture.js
@@ -1,8 +1,26 @@
+import * as d3 from 'd3';
 import Annotator from './annotator'
-
 class Texture extends Annotator {
     annotate(chart, target, style) {
         let svg = chart.svg();
+        d3.selection.prototype.moveToFront = function() {  
+            return this.each(function(){
+                this.parentNode.appendChild(this);
+            });
+        };
+        var config = {
+            "avatar_size" : 300
+        }
+        var defs = svg.append('svg:defs');
+        defs.append("svg:pattern")
+            .attr("id", "grump_avatar")
+            .attr("width", config.avatar_size)
+            .attr("height", config.avatar_size)
+            .attr("patternUnits", "userSpaceOnUse")
+            .append("svg:image")
+            .attr("xlink:href", style["background-image"])
+            .attr("x", 0)
+            .attr("y", 0);
         svg.selectAll(".mark")
             .filter(function(d) {
                 if (target.length === 0) {
@@ -14,7 +32,9 @@ class Texture extends Annotator {
                     }
                 }
                 return false
-            });
+            })
+            .style("fill", "url(#grump_avatar)")
+            .moveToFront();
     }
 }
 
