5cd054cf026b7178b69e97c48b9479ae5ecd8eb0
Three Many <xuxinyue@tongji.edu.cn>
2022-05-07 16:22:59 +0800

    feat(circle): add animation fade/wipe/flyin in circle annotation (#63)

--- src/vis/actions/annotations/circle.js
+++ src/vis/actions/annotations/circle.js
@@ -1,5 +1,6 @@
 import Annotator from './annotator'
 import Color from '../../visualization/color';
+import * as d3 from 'd3';
 
 const COLOR = new Color();
 
@@ -22,6 +23,7 @@ class Circle extends Annotator {
      * @return {void}
      */
     annotate(chart, target, style, animation) {
+        let animationType = animation ? animation.type : null;
         let svg = chart.svg();
         let selected = svg.selectAll(".mark")
             .filter(function (d) {
@@ -46,43 +48,187 @@ class Circle extends Annotator {
                     circleX = item.getAttribute("cx"),
                     circleY = item.getAttribute("cy");
 
-                svg.append("circle")
-                    .attr("fill", "none")
-                    .attr("stroke", function (d) {
-                        if ('color' in style) {
-                            return style['color']
-                        } else {
-                            return COLOR.ANNOTATION
-                        }
-                    })
-                    .attr("stroke-width", 2)
-                    .attr("x", circleX)
-                    .attr("y", circleY)
-                    .attr("transform", "translate(" + circleX + "," + circleY + ")")
-                    .transition()
-                    .attr("r", circleR)
-                    .duration('duration' in animation ? animation['duration'] : 0)
-                    .attr("fill-opacity", 1)
+                let arc = d3.arc()
+                    .innerRadius(circleR * 0.9)
+                    .outerRadius(circleR);
+                let circle_data = {
+                    "x": parseFloat(circleX),
+                    "y": parseFloat(circleY),
+                    "startAngle": 0,
+                    "endAngle": 2 * Math.PI
+                };
+
+                switch (animationType) {
+                    case "fade":
+                        svg.append("circle")
+                            .attr("fill", "none")
+                            .attr("stroke", function (d) {
+                                if ('color' in style) {
+                                    return style['color']
+                                } else {
+                                    return COLOR.ANNOTATION
+                                }
+                            })
+                            .attr("stroke-width", 2)
+                            .attr("x", circleX)
+                            .attr("y", circleY)
+                            .attr("transform", "translate(" + circleX + "," + circleY + ")")
+                            .attr("r", circleR)
+                            .attr("opacity", 0)
+                            .transition()
+                            .duration('duration' in animation ? animation['duration'] : 0)
+                            .attr("opacity", 1)
+                        break;
+
+                    case "wipe":
+                        svg.append("path")
+                            .attr("fill", function (d) {
+                                if ('color' in style) {
+                                    return style['color']
+                                } else {
+                                    return COLOR.ANNOTATION
+                                }
+                            })
+                            .attr("stroke", function (d) {
+                                if ('color' in style) {
+                                    return style['color']
+                                } else {
+                                    return COLOR.ANNOTATION
+                                }
+                            })
+                            .attr("stroke-width", 1)
+                            .attr("x", circleX)
+                            .attr("y", circleY)
+                            .attr("transform", "translate(" + circleX + "," + circleY + ")")
+                            .attr("r", circleR)
+                            .datum(circle_data)
+                            .attr("d", arc)
+                            .transition()
+                            .duration(animation.duration)
+                            .attrTween('d', function (d) {
+                                var i = d3.interpolate(d.startAngle, d.endAngle);
+                                return function (t) {
+                                    d.endAngle = i(t);
+                                    return arc(d);
+                                }
+                            });
+
+                        break;
+
+                    case "fly-in":
+                        const chartWidth = chart.width() - chart.margin().top;
+                        svg.append("circle")
+                            .attr("fill", "none")
+                            .attr("stroke", function (d) {
+                                if ('color' in style) {
+                                    return style['color']
+                                } else {
+                                    return COLOR.ANNOTATION
+                                }
+                            })
+                            .attr("stroke-width", 2)
+                            .attr("x", circleX)
+                            .attr("y", circleY)
+                            .attr("r", circleR)
+                            .attr("transform", "translate(" + chartWidth + "," + circleY + ")")
+                            .attr("x", 600)
+                            .transition()
+                            .duration('duration' in animation ? animation['duration'] : 0)
+                            .attr("transform", "translate(" + circleX + "," + circleY + ")")
+                        break;
+                    default:
+                        break;
+                }
 
             } else if (item.nodeName === "rect") {
                 let circleY = item.getAttribute("y"),
                     width = item.getAttribute("width"),
                     circleX = Number(item.getAttribute("x")) + width / 2,
                     circleR = width / 2 + padding;
-                svg.append("circle")
-                    .attr("fill", "none")
-                    .attr("stroke", function (d) {
-                        if ('color' in style) {
-                            return style['color']
-                        } else {
-                            return COLOR.ANNOTATION
-                        }
-                    })
-                    .attr("stroke-width", 2)
-                    .attr("x", circleX)
-                    .attr("y", circleY)
-                    .attr("transform", "translate(" + circleX + "," + circleY + ")")
-                    .attr("r", circleR)
+
+                let arc = d3.arc()
+                    .innerRadius(circleR * 0.9)
+                    .outerRadius(circleR);
+                let circle_data = {
+                    "x": parseFloat(circleX),
+                    "y": parseFloat(circleY),
+                    "startAngle": 0,
+                    "endAngle": 2 * Math.PI
+                };
+                switch (animationType) {
+                    case "fade":
+                        svg.append("circle")
+                            .attr("fill", "none")
+                            .attr("stroke", function (d) {
+                                if ('color' in style) {
+                                    return style['color']
+                                } else {
+                                    return COLOR.ANNOTATION
+                                }
+                            })
+                            .attr("stroke-width", 2)
+                            .attr("x", circleX)
+                            .attr("y", circleY)
+                            .attr("transform", "translate(" + circleX + "," + circleY + ")")
+                            .attr("r", circleR)
+                            .attr("opacity", 0)
+                            .transition()
+                            .duration('duration' in animation ? animation['duration'] : 0)
+                            .attr("opacity", 1)
+                        break;
+
+                    case "wipe":
+                        svg.append("path")
+                            .attr("fill", function (d) {
+                                if ('color' in style) {
+                                    return style['color']
+                                } else {
+                                    return COLOR.ANNOTATION
+                                }
+                            })
+                            .attr("stroke-width", 1)
+                            .attr("x", circleX)
+                            .attr("y", circleY)
+                            .attr("transform", "translate(" + circleX + "," + circleY + ")")
+                            .attr("r", circleR)
+                            .datum(circle_data)
+                            .attr("d", arc)
+                            .transition()
+                            .duration(animation.duration)
+                            .attrTween('d', function (d) {
+                                var i = d3.interpolate(d.startAngle, d.endAngle);
+                                return function (t) {
+                                    d.endAngle = i(t);
+                                    return arc(d);
+                                }
+                            });
+
+                        break;
+
+                    case "fly-in":
+                        const chartWidth = chart.width() - chart.margin().top;
+                        svg.append("circle")
+                            .attr("fill", "none")
+                            .attr("stroke", function (d) {
+                                if ('color' in style) {
+                                    return style['color']
+                                } else {
+                                    return COLOR.ANNOTATION
+                                }
+                            })
+                            .attr("stroke-width", 2)
+                            .attr("x", circleX)
+                            .attr("y", circleY)
+                            .attr("r", circleR)
+                            .attr("transform", "translate(" + chartWidth + "," + circleY + ")")
+                            .attr("x", 600)
+                            .transition()
+                            .duration('duration' in animation ? animation['duration'] : 0)
+                            .attr("transform", "translate(" + circleX + "," + circleY + ")")
+                        break;
+                    default:
+                        break;
+                }
             }
 
         });
