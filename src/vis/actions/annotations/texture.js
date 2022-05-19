e0ed354f5ccef50352187f5da2d42f666e799c30
pairone <40026218+pairone@users.noreply.github.com>
2022-05-19 19:25:23 +0800

    change texture to fit size (#115)

--- src/vis/actions/annotations/texture.js
+++ src/vis/actions/annotations/texture.js
@@ -2,7 +2,8 @@ import * as d3 from 'd3';
 import Annotator from './annotator';
 import { PieChart } from '../../charts';
 import { Unitvis } from '../../charts';
-
+import { LineChart } from '../../charts';
+import { Scatterplot } from '../../charts';
 
 /**
  * @description An annotator for filling texture.
@@ -30,19 +31,19 @@ class Texture extends Annotator {
             });
         };
 
-        var config = {
-            "texture_size" : 300
-        }
-        var defs = svg.append('svg:defs');
-        defs.append("svg:pattern")
-            .attr("id", "texture_background")
-            .attr("width", config.texture_size)
-            .attr("height", config.texture_size)
-            .attr("patternUnits", "userSpaceOnUse")
-            .append("svg:image")
-            .attr("xlink:href", style["background-image"])
-            .attr("x", 0)
-            .attr("y", 0);
+        // var config = {
+        //     "texture_size" : 300
+        // }
+        // var defs = svg.append('svg:defs');
+        // defs.append("svg:pattern")
+        //     .attr("id", "texture_background")
+        //     .attr("width", config.texture_size)
+        //     .attr("height", config.texture_size)
+        //     .attr("patternUnits", "userSpaceOnUse")
+        //     .append("svg:image")
+        //     .attr("xlink:href", style["background-image"])
+        //     .attr("x", 0)
+        //     .attr("y", 0);
 
             
         const uid = Date.now().toString() + Math.random().toString(36).substring(2);
@@ -62,6 +63,20 @@ class Texture extends Annotator {
         });
 
         if ("type" in animation && animation["type"] === "wipe") {
+            var configWipe = {
+                "texture_size" : 300
+            }
+            var defsWipe = svg.append('svg:defs');
+            defsWipe.append("svg:pattern")
+                .attr("id", "texture_background")
+                .attr("width", configWipe.texture_size)
+                .attr("height", configWipe.texture_size)
+                .attr("patternUnits", "userSpaceOnUse")
+                .append("svg:image")
+                .attr("xlink:href", style["background-image"])
+                .attr("x", 0)
+                .attr("y", 0);
+    
             focus_elements.nodes().forEach(one_element => {
                 const nodeName = one_element.nodeName;
                 if (nodeName === "rect") {
@@ -98,70 +113,193 @@ class Texture extends Annotator {
 
             focus_elements.style("fill", "url(#texture_background)"); 
 
-        } else if (chart instanceof Unitvis){
-            svg.selectAll(".mark")
-            .filter(function(d) {
-                if (target.length === 0) {
-                    return true
-                }
-                for (const item of target) {
-                    if (d[item.field] === item.value) {
-                        continue
-                    } else {
-                        return false
-                    }
-                }
-                return true
-            })
-            .transition()
-            .duration('duration' in animation ? animation['duration']: 0)
-            .attr("fill", (d, i) => {
-                    let defs = svg.append('svg:defs');
-                    defs.append("svg:pattern")
-                        .attr("id", "mark-background-image-" + i)
-                        .attr("width", 1)
-                        .attr("height", 1)
-                        .attr("patternUnits", "objectBoundingBox")
-                        .append("svg:image")
-                        .attr("xlink:href", style["background-image"])
-                        .attr("width", 2 * d.radius())
-                        .attr("height", 2 * d.radius())
-                        .attr("x", 0)
-                        .attr("y", 0);
-                    return "url(#mark-background-image-" + i + ")"
-                } 
-            )
-        }
+        } 
         else {
-            svg.selectAll(".mark")
-            .filter(function(d) {
-                if (target.length === 0) {
-                    return true
-                }
-                for (const item of target) {
-                    if (d[item.field] === item.value) {
-                        continue
-                    } else {
-                        return false
+            if (chart instanceof Unitvis){
+                svg.selectAll(".mark")
+                .filter(function(d) {
+                    if (target.length === 0) {
+                        return true
                     }
-                }
-                return true
-            })
-            .transition()
-            .duration('duration' in animation ? animation['duration']: 0)
-            .style("fill", "url(#texture_background)")
+                    for (const item of target) {
+                        if (d[item.field] === item.value) {
+                            continue
+                        } else {
+                            return false
+                        }
+                    }
+                    return true
+                })
+                .transition()
+                .duration('duration' in animation ? animation['duration']: 0)
+                .attr("fill", (d, i) => {
+                        const uid = Date.now().toString() + Math.random().toString(36).substring(2);
+                        let defs = svg.append('svg:defs');
+                        defs.append("svg:pattern")
+                            .attr("id", "unit-texture-image-" + uid)
+                            .attr("width", 1)
+                            .attr("height", 1)
+                            .attr("patternUnits", "objectBoundingBox")
+                            .append("svg:image")
+                            .attr("xlink:href", style["background-image"])
+                            .attr("width", 2 * d.radius())
+                            .attr("height", 2 * d.radius())
+                            .attr("x", 0)
+                            .attr("y", 0);
+                        return "url(#unit-texture-image-" + uid + ")"
+                    } 
+                )
+            }else if (chart instanceof LineChart){
+                svg.selectAll(".mark")
+                .filter(function(d) {
+                    if (target.length === 0) {
+                        return true
+                    }
+                    for (const item of target) {
+                        if (d[item.field] === item.value) {
+                            continue
+                        } else {
+                            return false
+                        }
+                    }
+                    return true
+                })
+                .transition()
+                .duration('duration' in animation ? animation['duration']: 0)
+                .attr("fill", (d, i) => {
+                        let dotRadius;
+                        focus_elements.nodes().forEach(one_element => {
+                            dotRadius = parseFloat(one_element.getAttribute("r"));
+                        })
+                        const uid = Date.now().toString() + Math.random().toString(36).substring(2);
 
-            focus_elements.nodes().forEach(one_element => {
-                if(chart instanceof PieChart){
+                        let defs = svg.append('svg:defs');
+                        defs.append("svg:pattern")
+                            .attr("id", "linechart-texture-image-" + uid)
+                            .attr("width", 1)
+                            .attr("height", 1)
+                            .attr("patternUnits", "objectBoundingBox")
+                            .append("svg:image")
+                            .attr("xlink:href", style["background-image"])
+                            .attr("width", 2 * dotRadius)
+                            .attr("height", 2 * dotRadius)
+                            .attr("x", 0)
+                            .attr("y", 0);
+                        return "url(#linechart-texture-image-" + uid + ")"
+                    } 
+                )
+            }else if (chart instanceof Scatterplot){
+                svg.selectAll(".mark")
+                .filter(function(d) {
+                    if (target.length === 0) {
+                        return true
+                    }
+                    for (const item of target) {
+                        if (d[item.field] === item.value) {
+                            continue
+                        } else {
+                            return false
+                        }
+                    }
+                    return true
+                })
+                .transition()
+                .duration('duration' in animation ? animation['duration']: 0)
+                .attr("fill", (d, i) => {
+                        let defsScatterplot = svg.append('svg:defs');
+                        const uid = Date.now().toString() + Math.random().toString(36).substring(2);
+                        defsScatterplot.append("svg:pattern")
+                            .attr("id", "scatterplot-texture-image-" + uid)
+                            .attr("width", 1)
+                            .attr("height", 1)
+                            .attr("patternUnits", "objectBoundingBox")
+                            .append("svg:image")
+                            .attr("xlink:href", style["background-image"])
+                            .attr("width", 2 * d.size())
+                            .attr("height", 2 * d.size())
+                            .attr("x", 0)
+                            .attr("y", 0);
+                        return "url(#scatterplot-texture-image-" + uid + ")"
+                    } 
+                )
+            }else if (chart instanceof PieChart){
+                var configPieChart = {
+                    "texture_size" : 300
+                }
+                var defsPieChart = svg.append('svg:defs');
+                defsPieChart.append("svg:pattern")
+                    .attr("id", "texture_background")
+                    .attr("width", configPieChart.texture_size)
+                    .attr("height", configPieChart.texture_size)
+                    .attr("patternUnits", "userSpaceOnUse")
+                    .append("svg:image")
+                    .attr("xlink:href", style["background-image"])
+                    .attr("x", 0)
+                    .attr("y", 0);
+        
+                var selectedMarks = svg.selectAll(".mark")
+                .filter(function(d) {
+                    if (target.length === 0) {
+                        return true
+                    }
+                    for (const item of target) {
+                        if (d[item.field] === item.value) {
+                            continue
+                        } else {
+                            return false
+                        }
+                    }
+                    return true
+                })
+                .transition()
+                .duration('duration' in animation ? animation['duration']: 0)
+                .style("fill", "url(#texture_background)")
+
+
+                focus_elements.nodes().forEach(one_element => {
                     let data_temp = one_element.__data__;
                     let data_x = data_temp.centroidX();
                     let data_y = data_temp.centroidY();
-                    d3.selectAll("pattern")
-                    .attr("x",(d,i)=>data_x-150)
+                selectedMarks.attr("x",(d,i)=>data_x-150)
                     .attr("y",(d,i)=>data_y-150)
+                })
+            }else{
+                var config = {
+                    "texture_size" : 300
                 }
-            })
-        }      
+                var defs = svg.append('svg:defs');
+                defs.append("svg:pattern")
+                    .attr("id", "texture_background")
+                    .attr("width", config.texture_size)
+                    .attr("height", config.texture_size)
+                    .attr("patternUnits", "userSpaceOnUse")
+                    .append("svg:image")
+                    .attr("xlink:href", style["background-image"])
+                    .attr("width", config.texture_size)
+                    .attr("height", config.texture_size)
+                    .attr("x", 0)
+                    .attr("y", 0);
+        
+                svg.selectAll(".mark")
+                .filter(function(d) {
+                    if (target.length === 0) {
+                        return true
+                    }
+                    for (const item of target) {
+                        if (d[item.field] === item.value) {
+                            continue
+                        } else {
+                            return false
+                        }
+                    }
+                    return true
+                })
+                .transition()
+                .duration('duration' in animation ? animation['duration']: 0)
+                .style("fill", "url(#texture_background)")
+
+            }      
+        }
     }
 }
 
