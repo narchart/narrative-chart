6c96739772998582658818b4d1b423dce388b853
sdq <sdq@tongji.edu.cn>
2022-05-26 17:57:10 +0800

    update to d3 timer

--- src/vis/pipeline.js
+++ src/vis/pipeline.js
@@ -1,3 +1,4 @@
+import * as d3 from 'd3';
 import { AddAnnotation } from './actions';
 
 /**
@@ -8,7 +9,7 @@ import { AddAnnotation } from './actions';
 class Pipeline {
     constructor() {
         this._actions = []
-        this._timeouts = []
+        this._timers = []
     }
 
     add(action) {
@@ -35,7 +36,7 @@ class Pipeline {
             if (action instanceof AddAnnotation) { 
                 delayTime += 100 // TODO: add a small time period for waiting movement of marks (fix in the future)
             }
-            this._timeouts.push(setTimeout(function(){
+            this._timers.push(d3.timeout(function(){
                 // Code to run after the pause
                 action.operate(vis);
             }, delayTime));
@@ -48,8 +49,9 @@ class Pipeline {
      * @returns {void}
      */
     stop() {
-        for (var i = 0; i < this._timeouts.length; i++) {
-            clearTimeout(this._timeouts[i]);
+        for (var i = 0; i < this._timers.length; i++) {
+            // clearTimeout(this._timers[i]);
+            this._timers[i].stop();
         }
     }
 }
