39bd507b06dfe83754ad1652c76906411ceb9f5c
qxy2000 <2536413141@qq.com>
2023-08-03 13:24:19 +0800

    Add the axis to the annotation target

--- src/vis/actions/annotations/glow.js
+++ src/vis/actions/annotations/glow.js
@@ -15,7 +15,7 @@ class Glow extends Annotator {
      * @description Fill target marks with color.
      * 
      * @param {Chart} chart src/vis/charts/chart.js
-     * @param {Array} target It describes the data scope of the annotation, which is defined by a list of filters: [{field_1: value_1}, ..., {field_k, value_k}]. By default, the target is the entire data.
+     * @param {Array} target It describes the data scope of the annotation, which is defined by a list of filters: [{axis_1}, {axis_2}, {field_1: value_1}, ..., {field_k, value_k}]. Where {axis} stands for target as a coordinate axis and {field, value} stands for target as a point. By default, the target is the entire data.
      * @param {{color: string}} style Style parameters of the annotation.
      * @param {{delay: number, duration: number}} animation Animation parameters of the annotation.
      * 
@@ -24,6 +24,7 @@ class Glow extends Annotator {
     annotate(chart, target, style, animation) {
         let svg = chart.svg();
         var defs = svg.append("defs");
+        var filteredTarget = target;
         var filter = defs.append("filter")
             .attr("id", "drop-shadow")
             .attr("height", "150%");
@@ -50,12 +51,100 @@ class Glow extends Annotator {
         feMerge.append("feMergeNode")
             .attr("in", "FillPaint");
 
+        // target is the axis(s) 
+        if (target.length !== 0) {
+            target.forEach(function(item) {
+                if ('axis' in item) {
+                  // Handle cases that contain "axis"
+                  console.log('Axis:', item.axis);
+                  if(item.axis === "x"){
+                    // domain path
+                    svg.select(".axis_x")
+                       .selectAll(".domain")
+                       .transition()
+                       .duration('duration' in animation ? animation['duration']: 0)
+                       .style("stroke",  function() {
+                           if ('color' in style) {
+                               return style['color']
+                           } else {
+                               return COLOR.ANNOTATION
+                           }
+                       })
+                       .attr("stroke-opacity", 1)
+                       .attr("stroke-width", 2)
+                       .style("filter", "url(#drop-shadow)")
+                       .attr("opacity",1)
+                    // tick
+                    svg.select(".axis_x")
+                       .selectAll(".tick")
+                       .selectAll("line")
+                       .transition()
+                       .duration('duration' in animation ? animation['duration']: 0)
+                       .style("stroke",  function() {
+                           if ('color' in style) {
+                               return style['color']
+                           } else {
+                               return COLOR.ANNOTATION
+                           }
+                       })
+                       .attr("stroke-opacity", 1)
+                       .attr("stroke-width", 2)
+                    //    .style("filter", "url(#drop-shadow)")
+                       .attr("opacity",1)
+                  }
+                  else if(item.axis === "y"){
+                    // domain path
+                    svg.select(".axis_y")
+                       .selectAll(".domain")
+                       .transition()
+                       .duration('duration' in animation ? animation['duration']: 0)
+                       .style("stroke",  function() {
+                           if ('color' in style) {
+                               return style['color']
+                           } else {
+                               return COLOR.ANNOTATION
+                           }
+                       })
+                       .attr("stroke-opacity", 1)
+                       .attr("stroke-width", 2)
+                       .style("filter", "url(#drop-shadow)")
+                       .attr("opacity",1)
+                    // tick
+                    svg.select(".axis_y")
+                       .selectAll(".tick")
+                       .selectAll("line")
+                       .transition()
+                       .duration('duration' in animation ? animation['duration']: 0)
+                       .style("stroke",  function() {
+                           if ('color' in style) {
+                               return style['color']
+                           } else {
+                               return COLOR.ANNOTATION
+                           }
+                       })
+                       .attr("stroke-opacity", 1)
+                       .attr("stroke-width", 2)
+                    //    .style("filter", "url(#drop-shadow)")
+                       .attr("opacity",1)
+                  }
+                }
+            })
+            filteredTarget = target.filter(function(item) {
+                if ('axis' in item) {
+                  return false; // Returning false excludes axis objects from the new array
+                } else {
+                  return true; 
+                }
+              });
+        }
+
+        // target is the point(s) 
         svg.selectAll(".mark")
             .filter(function(d) {
-                if (target.length === 0) {
+                if (filteredTarget.length === 0) {
                     return true
                 }
-                for (const item of target) {
+                for (const item of filteredTarget) {
                     if (d[item.field] === item.value) {
                         continue
                     } else {
